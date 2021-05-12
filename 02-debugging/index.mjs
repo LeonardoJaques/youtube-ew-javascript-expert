6c7dbe65ea17b24f1e66c0a0be6072f19bbdc648
LeonardoJaques <jaques.projetos@outlook.com>
2021-05-12 01:04:54 -0300

    Live #2 - Técnicas de Debugging para Node.js - REPL.it, VSCode & Chrome - Pré #SemanaJSExpert

---
+++ 02-debugging/index.mjs
@@ -0,0 +1,28 @@
+import http from 'http';
+
+// Error.stackTraceLimit = 2;
+// function selfCaller(count = 0) {
+//   console.count('selfCaller');
+//   if (count >= 1000) throw Error('Its not ok');
+//   return selfCaller((count += 1));
+// }
+// selfCaller();
+
+function netSalary({ discount, salary }) {
+  const percent = discount / 100;
+  const cost = salary * percent;
+  const result = salary - cost;
+
+  return result;
+}
+
+http
+  .createServer((req, res) => {
+    const url = req.url.replace('/', '');
+    const params = new URLSearchParams(url);
+    const data = Object.fromEntries(params);
+    const result = netSalary(data);
+    // debugger;
+    res.end(`O seu salario final é ${result}`);
+  })
+  .listen(3000, () => console.log('app running at 3000'));
